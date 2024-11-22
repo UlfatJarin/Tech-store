0951feccc35fad3f00bb2da8cdebb7412cc7bded
UlfatJarin <richiulfat@gmail.com>
2024-11-23 01:03:47 +0600

    skjfnsl

--- src/Page/ContactUs.jsx
+++ src/Page/ContactUs.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import Input from '../layer/Input'
+import Container from '../layer/Container'
 
 const ContactUs = () => {
-  return (
-    <div className='Ccontainer'>
-        <div>
-            <h2 className='title2'>Contact Us</h2>
-            <p className='p5 !text-base'>We love hearing from you, our Shop customers.<br/>Please contact us and we will make sure to get back to you as soon as we possibly can.</p>
+    return (
+        <div className=''>
+            <Container>
+                <div>
+                    <h2 className='title2 mb-4'>Contact Us</h2>
+                    <p className='p5 !text-base'>We love hearing from you, our Shop customers.<br />Please contact us and we will make sure to get back to you as soon as we possibly can.</p>
+                </div>
+                <div className='grid grid-cols-3 gap-16 mt-5'>
+                    <div className='col-span-2 grid grid-cols-2 gap-7'>
+                        <Input className='max-w-[449px]' labeltext='Your Name' id='name' type='text' placeholder='Your Name' />
+                        <Input className='max-w-[449px]' labeltext='Your Email ' id='email' type='email' placeholder='Your Email' />
+                        <Input className='max-w-[449px]' labeltext='Your Phone Number' id='number' type='number' placeholder='Your Phone' />
+                        <div className='col-span-2'>
+                            <Input className='max-w-[925px]' className1='h-[236px] ' labeltext='What’s on your mind? ' id='comment' type='text' placeholder='Jot us a note and we’ll get back to you as quickly as possible' />
+                        </div>
+                    </div>
+                    <div className='bg-bg-secondary'>
+                        <p>jsnfkjsnfkjnsfsafsf</p>
+                    </div>
+                </div>
+            </Container>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ContactUs
--- src/layer/Input.jsx
+++ src/layer/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Input = ({labeltext, inputtext ,placeholder, type,id ,className}) => {
+const Input = ({labeltext, inputtext ,placeholder, type,id ,className,className1}) => {
   return (
     <div className={`max-w-[565px] ${className}`} >
         <lebel className="p4 !font-semibold capitalize ">{labeltext} </lebel><br/>
-        <input className={`w-full mt-3 p5 !text-sm capitalize text-bg-secondary p-4 border border-[#A2A6B0] rounded-[4px] `} id={id} type={type} placeholder={placeholder}>{inputtext}</input>
+        <input className={`w-full mt-3 p5 !text-sm capitalize text-bg-secondary p-4 border border-[#A2A6B0] rounded-[4px] ${className1}`} id={id} type={type} placeholder={placeholder}>{inputtext}</input>
     </div>
   )
 }
